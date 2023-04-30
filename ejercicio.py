d6d4a5ad4885b3ba678baa90e6b51dba2e2a57db
andmansim <91722324+andmansim@users.noreply.github.com>
2023-04-30 19:10:41 +0200

    personas

--- ejercicio.py
+++ ejercicio.py
@@ -1,5 +1,7 @@
 global store
-codes = ('1', '2', '3')
-store = {'1':{'name': 'Papel', 'required': 'Tabaco y Fósforos', 'flag': False, 'request': None},
-         '1':{'name': 'Tabaco', 'required': 'Papel y Fósforos', 'flag': False, 'request': None},
-         '1':{'name': 'Fósforo', 'required': 'Papel y Tabaco', 'flag': False, 'request': None},}
+codes = ('1', '2', '3', '4', '5') #Lo que tiene cada persona y lo que necesita
+store = {'1':{'name': 'Papel', 'required': 'Tabaco, Fósforos, Filtros, Cerillas', 'flag': False, 'request': None},
+         '2':{'name': 'Tabaco', 'required': 'Papel, Fósforos, Filtros, Cerillas', 'flag': False, 'request': None},
+         '3':{'name': 'Fósforo', 'required': 'Papel, Tabaco, Filtros, Cerillas', 'flag': False, 'request': None},
+         '4':{'name': 'Filtros', 'required': 'Papel, Tabaco, Fósforos, Cerillas', 'flag': False, 'request': None},
+         '5':{'name': 'Cerillas', 'required': 'Papel, Tabaco, Filtros, Fósforos', 'flag': False, 'request': None}}
