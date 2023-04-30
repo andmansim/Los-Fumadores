e1b1e54a7cc53f988998d3f6247b044d4728c6e5
andmansim <91722324+andmansim@users.noreply.github.com>
2023-04-30 19:42:14 +0200

    inicio

--- proveedor.py
+++ proveedor.py
@@ -3,3 +3,25 @@ import time
 from random import choice
 import socketserver
 from almacenar import codes, tamanio, store, tiempo_dormir, tiempo_fumar
+from utils import _print
+
+global smoke
+smoke = False
+global smoke_code
+
+class MyTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+class MTCPServerHandler(socketserver.BaseRequestHandler):
+    bufer = ''
+    def proceso(self):
+        while True:
+            mensaje = self.request.recv(tamanio).decode('UTF-8')
+            if mensaje == 'need':
+                _print('{}: Necesito {}'.format(store.get(self.code)['name'], store.get(self.code)['required']))
+                if self.smoke_released:
+                    self.smoke_released = False
+                    global smoke
+                    smoke = False
+                    
+            
