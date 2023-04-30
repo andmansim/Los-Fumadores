c393caf2a0bbdb36f8972a759d7bda2cb1c9a440
andmansim <91722324+andmansim@users.noreply.github.com>
2023-04-30 19:07:14 +0200

    datos iniciales

---
+++ ejercicio.py
@@ -0,0 +1,5 @@
+global store
+codes = ('1', '2', '3')
+store = {'1':{'name': 'Papel', 'required': 'Tabaco y Fósforos', 'flag': False, 'request': None},
+         '1':{'name': 'Tabaco', 'required': 'Papel y Fósforos', 'flag': False, 'request': None},
+         '1':{'name': 'Fósforo', 'required': 'Papel y Tabaco', 'flag': False, 'request': None},}
