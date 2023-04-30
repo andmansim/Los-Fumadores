8645918e07c2bd0a6e180646d7839163534c6d11
andmansim <91722324+andmansim@users.noreply.github.com>
2023-04-30 19:22:46 +0200

    inicio proceso

--- fumador.py
+++ fumador.py
@@ -1,3 +1,16 @@
 import socket
 import time
 from almacenar import tamanio, store, tiempo_fumar, tiempo_dormir
+from utils import _print
+
+def proceso(c, r):
+    mensaje = ''
+    while True:
+        if mensaje != 'ack':
+            _print('Esperando {}!'.format(store.get(c)['required']))
+            r.send('need'.encode('UTF-8'))
+        
+        mensaje = r.recv(tamanio).decode('UTF-8')
+        if mensaje == 'enable':
+            _print('Servido!')
+            
